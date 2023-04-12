a5ad8b295b4f213250b2da4bf11687ea679d7347
Prabin Bhatt <bprabin811@gmail.com>
2023-04-12 14:08:50 +0545

    database added

--- app.py
+++ app.py
@@ -1,16 +1,57 @@
 from flask import Flask, render_template,request
 from pymongo import MongoClient
+from PIL import Image
+import base64
+import io
+# from db import retrive_data
 
 client = MongoClient('mongodb+srv://prabin:bprabin@cluster0.2phmxej.mongodb.net/test')
 db = client['pro']
 
-
+name=[
+    "KidzWorld: Learn With Fun",
+    "Helix-Infinite",
+    "ImageLab-AI: Image Colorization using CNN",
+    "Multi-Store E-mart for Vegetables",
+    "Depression Analysis using Chatbot",
+    "Churn Prediction System",
+]
 app = Flask(__name__,static_folder='static',template_folder='templates')
 
+items=[]
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html')
+    try:
+        items.clear()
+        for i in range(len(name)):
+            image_data = db.mydatabse.find_one({'name': name[i]})['image_data']
+            project_link=db.mydatabse.find_one({'name': name[i]})['link']
+            project_type=db.mydatabse.find_one({'name': name[i]})['type']
+            image = Image.open(io.BytesIO(image_data))
+            img_io = io.BytesIO()
+            image.save(img_io, format='PNG')
+            img_io.seek(0)
+            img_str = base64.b64encode(img_io.getvalue()).decode()
+
+            if project_type == 'app':
+                actionType = 'Download'
+            elif project_type == 'game':
+                actionType = 'Download'
+            else:
+                actionType = 'View Project'
+
+            item = {
+                'image': img_str,
+                'name': name[i],
+                'action': actionType,
+                'link': project_link,
+            }
+            items.append(item) 
+        return render_template('index.html',items=items)
+    except:
+        return '<p> Something Went Wrong, Please try again later.</p>'
+                            
 
 
 @app.route('/submit_form', methods=['POST'])
@@ -25,4 +66,5 @@ def submit_form():
         'email': email,
         'message': message
     })
+
     return render_template('index.html')
--- static/app.js
+++ static/app.js
@@ -1,39 +1,82 @@
-const sections = document.querySelectorAll('.section');
-const sectBtns = document.querySelectorAll('.controlls');
-const sectBtn = document.querySelectorAll('.control');
-const allSections = document.querySelectorAll('.main-content');
-// console.log(sectBtn.length);
+// const followIcons = document.querySelectorAll('.follow a i');
 
-function PageTransition() {
-    for (let i = 0; i < sectBtn.length; i++) {
-        sectBtn[i].addEventListener('click', function () {
-            let currentBtn = document.querySelectorAll('.active-btn');
-            currentBtn[0].className = currentBtn[0].className.replace('active-btn', '');
-            this.className += ' active-btn';
-        })
-    }
+// // Cache the window dimensions
+// const windowHeight = window.innerHeight;
+// const windowWidth = window.innerWidth;
 
-    //Sections Active class
-    allSections.forEach((section) =>
-        section.addEventListener("click", (e) => {
-            const id = e.target.dataset.id;
-            // console.log(id);
+// // Add hardware acceleration to icons
+// followIcons.forEach((icon) => {
+//   icon.style.transform = 'translate3d(0, 0, 0)';
+// });
 
-            if (id) {
-                sectBtns.forEach((btn) => {
-                    btn.classList.remove("active");
-                });
-                e.target.classList.add("active");
-                sections.forEach((section) => {
-                    section.classList.remove("active");
-                });
+// function getRandomNumber(min, max) {
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
 
-                const element = document.getElementById(id);
-                console.log(element);
-                element.classList.add("active");
-            }
-        })
-    );
-}
+// function moveIcon(icon) {
+//   // Cache the current position of the icon
+//   const currentPosition = icon.getBoundingClientRect();
 
-PageTransition();
+//   // Generate random coordinates for the icon to move towards
+//   const newX = getRandomNumber(0, windowWidth - 50);
+//   const newY = getRandomNumber(0, windowHeight - 50);
+
+//   // Define the animation function to move the icon towards the new position
+//   function moveTowards() {
+//     // Calculate the distance between the current position and the target position
+//     const distanceX = newX - currentPosition.x;
+//     const distanceY = newY - currentPosition.y;
+
+//     // Calculate the amount of movement for each frame
+//     const moveX = (distanceX / 5);
+//     const moveY = (distanceY / 100);
+
+//     // Move the icon by the calculated amount
+//     icon.style.transform = `translate(${moveX}px, ${moveY}px)`;
+
+//     // If the icon hasn't reached the target position, continue the animation
+//     if (Math.abs(distanceX) > 1 || Math.abs(distanceY) > 1) {
+//       requestAnimationFrame(moveTowards);
+//     } else {
+//       // If the icon has reached the target position, delay for 2 seconds and then return to the original position
+//       setTimeout(() => {
+//         returnToPosition();
+//       }, 2000);
+//     }
+//   }
+
+//   // Define the animation function to return the icon to its original position
+//   function returnToPosition() {
+//     // Cache the current position of the icon
+//     const currentPosition = icon.getBoundingClientRect();
+
+//     // Calculate the distance between the current position and the original position
+//     const distanceX = 0 - currentPosition.x;
+//     const distanceY = 0 - currentPosition.y;
+
+//     // Move the icon by the calculated amount
+//     icon.style.transform = `translate(${distanceX}px, ${distanceY}px)`;
+
+//     // If the icon hasn't reached the original position, continue the animation
+//     if (Math.abs(distanceX) > 1 || Math.abs(distanceY) > 1) {
+//       requestAnimationFrame(returnToPosition);
+//     }
+//   }
+
+//   // Start the animation to move the icon towards the new position
+//   moveTowards();
+// }
+
+// function animateIcons(icons) {
+//   icons.forEach((icon) => {
+//     // Move the icon to a random position initially
+//     moveIcon(icon);
+
+//     // Every 5 seconds, move the icon to a new random position
+//     setInterval(() => {
+//       moveIcon(icon);
+//     }, 2000);
+//   });
+// }
+
+// animateIcons(followIcons);
