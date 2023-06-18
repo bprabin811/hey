c372eaf2055d6d515e8f48cd41bed76e76dcfb23
Prabin Bhatta <106478486+bprabin811@users.noreply.github.com>
2023-06-18 20:29:51 +0545

    Update app.py

--- app.py
+++ app.py
@@ -54,16 +54,19 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/generate-code', methods=['GET'])
 def generate_profile_card():
-    prompt_text = request.args.get('prompt', ''+' write HTML and CSS in same file index.html')
+    try:
+        prompt_text = request.args.get('prompt', ''+' write HTML and CSS in same file index.html')
 
-    response = openai.Completion.create(
-        engine='text-davinci-003',
-        prompt=prompt_text,
-        max_tokens=2500
-    )
+        response = openai.Completion.create(
+            engine='text-davinci-003',
+            prompt=prompt_text,
+            max_tokens=2500
+        )
 
-    generated_text = response.choices[0].text.strip()
-    return jsonify({'gen_code': generated_text})
+        generated_text = response.choices[0].text.strip()
+        return jsonify({'gen_code': generated_text})
+    except:
+        return jsonify({'gen_code': '<p>Due to high triffic, we are unable to process your request, please try again later.</p>'})
 
 # def generate_response(prompt):
 #     response = openai.Completion.create(
